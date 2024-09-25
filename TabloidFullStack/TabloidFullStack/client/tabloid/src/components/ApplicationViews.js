3c0b89b0191a13d87523ae108df1adbd96b0a04f
Daniel Miller <dmiller3830@gmail.com>
2024-09-25 11:37:33 -0400

    fixed errors

--- TabloidFullStack/TabloidFullStack/client/tabloid/src/components/ApplicationViews.js
+++ TabloidFullStack/TabloidFullStack/client/tabloid/src/components/ApplicationViews.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Hello from "./Hello";
-import { TagDetails } from "./Tags/TagDetails.js";
-import { TagList } from "./Tags/TagList.js";
-import { UpdateTagForm } from "./Tags/UpdateTagForm.js";
 import { CreateTagForm } from "./Tags/CreateTagForm.js";
+import { UpdateTagForm } from "./Tags/UpdateTagForm.js";
+import { TagList } from "./Tags/TagList.js";
+import { TagDetails } from "./Tags/TagDetails.js";
 
 export default function ApplicationViews() {
 
