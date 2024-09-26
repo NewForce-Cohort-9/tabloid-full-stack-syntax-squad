882b259bc13d6af02e0137b130cc8f880ae30aa6
Jacob Dorris <jcbdrrs@gmail.com>
2024-09-26 13:49:32 -0500

    checking dan's branch

--- TabloidFullStack/TabloidFullStack/client/tabloid/src/App.js
+++ TabloidFullStack/TabloidFullStack/client/tabloid/src/App.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import { BrowserRouter as Router } from "react-router-dom";
-import Header from "./Components/Header";
-import ApplicationViews from "./Components/ApplicationViews";
-import { useEffect } from 'react';
-import Authorize from './Components/Authorize';
-=======
 import React, { useState, useEffect } from 'react';
 import Header from "./components/Header";
 import ApplicationViews from "./components/ApplicationViews";
 import Authorize from './components/Authorize';
->>>>>>> main
 
 function App() {
     const [isLoggedIn, setIsLoggedIn] = useState(true);
