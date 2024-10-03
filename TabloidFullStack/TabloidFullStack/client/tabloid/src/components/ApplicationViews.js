dbdd82a124b5adcc309960232dc8f81d84511f12
Daniel Miller <dmiller3830@gmail.com>
2024-10-03 10:20:27 -0400

    Fixed Errors

--- TabloidFullStack/TabloidFullStack/client/tabloid/src/components/ApplicationViews.js
+++ TabloidFullStack/TabloidFullStack/client/tabloid/src/components/ApplicationViews.js
@@ -3,6 +3,8 @@ import { Route, Routes } from "react-router-dom";
 import PostList from "./PostList";
 import { TagList } from "./Tags/TagList.js";
 import PostDetail from "./PostDetail.js";
+import { CreateTagForm } from "./Tags/CreateTagForm.js";
+import { TagDetails } from "./Tags/TagDetails.js";
 
 const ApplicationViews = () => {
   // return (
@@ -34,24 +36,5 @@ const ApplicationViews = () => {
    );
  
 }
-      {/* Route for displaying the detail view of a post */}
-import PostDetail from "./PostDetail";
-import { CreateTagForm } from "./Tags/CreateTagForm";
-import { TagList } from "./Tags/TagList";
-import { UpdateTagForm } from "./Tags/UpdateTagForm";
-import { TagDetails } from "./Tags/TagDetails";
 
-const ApplicationViews = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<PostList />} />
-      <Route path="/post/:id" element={<PostDetail />} />
-      <Route path="/tags/create" element={<CreateTagForm />} />
-      <Route path="/tags/:update" element={<UpdateTagForm />} />
-      <Route path="/tags" element={<TagList />} />
-      <Route path="/tags/:id" element={<TagDetails />} />
-    </Routes>
-  );
-};
-
-export default ApplicationViews;
+export default ApplicationViews
